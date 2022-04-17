176513981b5e6bd365a785ae0a31bab7278de2be
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-04-17 08:02:50 -0500

    Move descriptors character vector to functions file.

--- functions.R
+++ functions.R
@@ -3,3 +3,16 @@ filename2df <- function(filename) {
         stringsAsFactors = FALSE)  # , na.strings="null"
     return(df)
 }
+
+descrip <- c('adher', 'aggress', 'agitat', 'angry', 'challenging', 'combative',
+             'complian', 'comply', 'confront', 'cooperat', 'defensive',
+             'exaggerate', 'hysterical', 'pleasant', 'refuse', 'resist')
+
+has_any_descriptor <- function(string) {
+  for (pattern in descrip) {
+    if (grepl(pattern, string)) {
+      return(TRUE)
+    }
+  }
+  return(FALSE)
+}
