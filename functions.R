02f8152b0bc91e01c0cbc39d1ff3cb156dcc52d7
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-04-19 06:41:19 -0500

    much broader descriptor list

--- functions.R
+++ functions.R
@@ -4,9 +4,22 @@ filename2df <- function(filename) {
     return(df)
 }
 
-descrip <- c('adher', 'aggress', 'agitat', 'angry', 'challenging', 'combative',
-             'complian', 'comply', 'confront', 'cooperat', 'defensive',
-             'exaggerat', 'hysterical', 'unpleasant', 'refus', 'resist')
+descrip <- c(
+  'adhere', 'adherence', 'adhering', 'adherent', 'nonadherent', 'non-adherent',
+  'aggressive', 'agitated', 'angry', 'challenging', 'combative',
+  'compliance', 'comply', 'compliant', 'complying', 'noncompliant', 'non-compliant', 'noncompliance', 'non-compliance',
+  'confront',
+  'cooperate', 'cooperating', 'uncooperative', 'noncooperative', 'non-cooperative',
+  'defensive',
+  'exaggerate', 'exaggerating', 'exaggerated',
+  'hysterical', 'unpleasant',
+  'refuse', 'refusing',
+  'resist', 'resisting', 'resisted'
+)
+
+# (non-)adherent, aggressive, agitated, angry, challenging, combative,
+# (non-)compliant, confront, (non-)cooperative, defensive, exaggerate,
+# hysterical, (un-)pleasant, refuse, and resist.
 
 count_descriptors <- function(charvec) {
   n_found = as.numeric(grepl('asdfasdf', charvec))
